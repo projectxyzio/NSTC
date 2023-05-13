3053f3978a5caf949680c7b6a7178ca626716e8b
Shamil <shamil@shamils-MacBook-Pro.local>
2023-05-13 10:29:00 +0530

    table format

--- KPI_COMPARISON/VPLA_Multiple_Appium_Element.py
+++ KPI_COMPARISON/VPLA_Multiple_Appium_Element.py
@@ -334,9 +334,10 @@ class ExpediaTest(unittest.TestCase):
                 # print(f"{category_two} : ",data_set_two[x], "Seconds")
             if add_vpla_label and add_element_label:
                 pass
+            kpi_table_data.append(["","",""])
                 # print(f"Percentage Difference between {category_two} and {category_one} (Accuracy) :",( abs((data_set_two[x] - data_set_one[x])/(data_set_two[x] + data_set_one[x])) * 100) / data_set_one[x],"%")
         print("\n")
-        print(tabulate(kpi_table_data,headers=["KPI Name","Measurement Technique","Duration(In Seconds)"]))
+        print(tabulate(kpi_table_data,headers=["KPI Name","Measurement Technique","Duration(In Seconds)"],tablefmt="simple_grid"))
         
             
 
